19daf0f2b6633dbb0ff23dd2763c81f43ae31259
Evan Norton <evan@retro-mmo.com>
2023-02-07 02:32:24 -0500

    remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "../utils/api";
