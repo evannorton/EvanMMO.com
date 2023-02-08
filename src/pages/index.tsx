c2679d99b87a76f74d17f0ee140cf6fa2cf5ea47
Evan Norton <evan@retro-mmo.com>
2023-02-08 02:11:44 -0500

    remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from "next";
 import Page from "../components/Page";
 import { Title } from "@mantine/core";
-import { useEffect } from "react";
 
 const Home: NextPage = () => {
   return (
