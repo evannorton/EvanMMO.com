6014976a55f353026f714ca2f2a19c3682acdbcc
Evan Norton <evan@retro-mmo.com>
2023-02-07 23:00:45 -0500

    Page component

---
+++ src/components/Page.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Page: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>EvanMMO</title>
+        <meta
+          name="description"
+          content="A hub for EvanMMO's content creation and game development"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <div className="flex flex-col items-center gap-2">
+            <AuthShowcase />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+const AuthShowcase: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
+
+export default Page;
