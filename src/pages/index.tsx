8590963dd3abef64df683786b5c65372e60a3a5b
Evan Norton <evan@retro-mmo.com>
2023-02-07 21:51:43 -0500

    remove unneeded import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "../utils/api";
-
 const Home: NextPage = () => {
   return (
     <>
