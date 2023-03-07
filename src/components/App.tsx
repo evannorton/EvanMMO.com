d188781baaa4c523536ff351189d48eef7fda680
Evan Norton <evan@retro-mmo.com>
2023-03-07 04:26:18 -0500

    hide header on mobile

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,11 @@
-import { AppShell, Anchor, Button, Header } from "@mantine/core";
+import {
+  AppShell,
+  Anchor,
+  Button,
+  Header,
+  MediaQuery,
+  Box,
+} from "@mantine/core";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 interface Props {
@@ -8,85 +15,119 @@ interface Props {
 
 const App: React.FC<Props> = ({ children, streamIsLive }) => {
   const { data: sessionData } = useSession();
+  const links = [
+    {
+      text: "Livestream",
+      href: "/#livestream",
+      isShown: streamIsLive,
+    },
+    {
+      text: "Videos",
+      href: "/#videos",
+      isShown: true,
+    },
+    {
+      text: "Games",
+      href: "/#games",
+      isShown: true,
+    },
+  ];
   return (
     <AppShell
       fixed
       header={
         <Header
           style={{ alignItems: "center" }}
-          display="flex"
           height={60}
           p="md"
+          display="flex"
         >
-          <Anchor
-            sx={{
-              "&:hover": {
-                opacity: ".75",
-              },
+          <MediaQuery
+            smallerThan="sm"
+            styles={{
+              display: "none",
             }}
-            href="/"
-            color="gray.0"
-            size={24}
-            weight="bold"
-            underline={false}
-            mr="lg"
           >
-            EvanMMO
-          </Anchor>
-          {streamIsLive && (
-            <Anchor
-              sx={{
-                "&:hover": {
-                  opacity: ".75",
-                },
-              }}
-              href="/#livestream"
-              color="gray.4"
-              size={20}
-              weight="bold"
-              mr="xs"
-              underline={false}
-            >
-              Livestream
-            </Anchor>
-          )}
-          <Anchor
-            sx={{
-              "&:hover": {
-                opacity: ".75",
-              },
+            <Box style={{ width: "100%", alignItems: "center" }} display="flex">
+              <Anchor
+                sx={{
+                  "&:hover": {
+                    opacity: ".75",
+                  },
+                }}
+                href="/"
+                color="gray.0"
+                size={24}
+                weight="bold"
+                underline={false}
+                mr="lg"
+              >
+                EvanMMO
+              </Anchor>
+              {links
+                .filter((link) => link.isShown)
+                .map((link) => (
+                  <Anchor
+                    sx={{
+                      "&:hover": {
+                        opacity: ".75",
+                      },
+                    }}
+                    href={link.href}
+                    color="gray.4"
+                    size={20}
+                    weight="bold"
+                    mr="xs"
+                    underline={false}
+                    key={link.href}
+                  >
+                    {link.text}
+                  </Anchor>
+                ))}
+              <Button
+                color={sessionData ? "red" : "green"}
+                style={{ marginLeft: "auto" }}
+                onClick={
+                  sessionData ? () => void signOut() : () => void signIn()
+                }
+              >
+                {sessionData ? "Sign out" : "Sign in"}
+              </Button>
+            </Box>
+          </MediaQuery>
+          <MediaQuery
+            largerThan="sm"
+            styles={{
+              display: "none",
             }}
-            href="/#videos"
-            color="gray.4"
-            size={20}
-            weight="bold"
-            mr="xs"
-            underline={false}
           >
-            Videos
-          </Anchor>
-          <Anchor
-            sx={{
-              "&:hover": {
-                opacity: ".75",
-              },
-            }}
-            href="/#games"
-            color="gray.4"
-            size={20}
-            weight="bold"
-            mr="xs"
-            underline={false}
-          >
-            Games
-          </Anchor>
-          <Button
-            color={sessionData ? "red" : "green"}
-            style={{ marginLeft: "auto" }}
-            onClick={sessionData ? () => void signOut() : () => void signIn()}
-          >
-            {sessionData ? "Sign out" : "Sign in"}
-          </Button>
+            <Box style={{ width: "100%", alignItems: "center" }} display="flex">
+              <Anchor
+                sx={{
+                  "&:hover": {
+                    opacity: ".75",
+                  },
+                }}
+                href="/"
+                color="gray.0"
+                size={24}
+                weight="bold"
+                underline={false}
+                mr="lg"
+              >
+                EvanMMO
+              </Anchor>
+              <Button
+                color={sessionData ? "red" : "green"}
+                style={{ marginLeft: "auto" }}
+                onClick={
+                  sessionData ? () => void signOut() : () => void signIn()
+                }
+              >
+                {sessionData ? "Sign out" : "Sign in"}
+              </Button>
+            </Box>
+          </MediaQuery>
         </Header>
       }
     >
