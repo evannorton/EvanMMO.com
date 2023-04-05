8aac2808d571305fd33ac38ba7f52116e27b4183
Evan Norton <evan@retro-mmo.com>
2023-04-05 03:32:17 -0400

    increase discord embed height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -320,7 +320,7 @@ const Home: NextPage = () => {
             Community
           </Title>
           <iframe
-            style={{ border: "none", borderRadius: "0.5rem", height: "25rem" }}
+            style={{ border: "none", borderRadius: "0.5rem", height: "30rem" }}
             src="https://discordapp.com/widget?id=543159733821898773&theme=dark"
             width="100%"
             sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
