0fd2c089790dabb2d08539cbc7e209a2816b7a95
Evan Norton <evan@retro-mmo.com>
2023-12-19 14:15:47 -0600

    Fix dates

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,5 @@
 export const getFormattedDateString = (date: Date) =>
-  `${date.getFullYear()}-${`${date.getMonth() + 1}`.padStart(
+  `${date.getUTCFullYear()}-${`${date.getUTCMonth() + 1}`.padStart(
     2,
     "0"
-  )}-${`${date.getDate()}`.padStart(2, "0")}`;
+  )}-${`${date.getUTCDate()}`.padStart(2, "0")}`;
